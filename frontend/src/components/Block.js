b533724704e3522c1b88690c28bd2a37a9bd2aa4
Gennaro Maida <gtm1235@gmail.com>
2021-10-30 22:39:35 -0400

    Early formatiing of block data

---
+++ frontend/src/components/Block.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { MILLISECONDS_PY } from '../config'
+import Transaction from "./Transaction";
+
+function Block({ block }) {
+    const { timestamp, hash, data } = block
+    const hashDisplay = `${hash.substring(0, 15)}...`
+    const timestampDisplay = new Date(timestamp / MILLISECONDS_PY).toLocaleDateString();
+
+    return (
+        <div className="Block">
+            <div>Hash: {hashDisplay}</div>
+            <div>Timestamp: {timestampDisplay}</div>
+            <div>
+                {
+                    data.map(transaction => (
+                        <div key={Transaction.id}>
+                            <hr />
+                            <Transaction transaction={transaction}/>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Block;
