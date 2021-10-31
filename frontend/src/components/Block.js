150cf022672d2d288fb1e33f6066bb4051a067e6
Gennaro Maida <gtm1235@gmail.com>
2021-10-30 23:21:12 -0400

    Added additonal formatting and ability to show more or less transaction information

--- frontend/src/components/Block.js
+++ frontend/src/components/Block.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from 'react-bootstrap'
 import { MILLISECONDS_PY } from '../config'
 import Transaction from "./Transaction";
 
+function ToggleTransactionDisplay({ block }) {
+    const [displayTransaction, setDisplayTransaction] = useState(false);
+    const { data } = block;
+
+    const toggleDisplayTransaction = () => {
+        setDisplayTransaction(!displayTransaction);
+    }
+
+    if (displayTransaction) {
+        return (
+            <div>
+                {
+                    data.map(transaction => (
+                        <div key={Transaction.id}>
+                            <hr />
+                            <Transaction transaction={transaction} />
+                        </div>
+                    ))
+                };
+                <div>
+                    <br />
+                    <Button
+                        variant="danger"
+                        size="sm"
+                        onClick={toggleDisplayTransaction}
+                    >
+                        Show Less
+                    </Button>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <br />
+            <Button
+                variant="Danger"
+                size="sm"
+                onClick={toggleDisplayTransaction}
+            >
+                Show More
+            </Button>
+        </div>
+    )
+}
+
 function Block({ block }) {
-    const { timestamp, hash, data } = block
+    const { timestamp, hash } = block
     const hashDisplay = `${hash.substring(0, 15)}...`
     const timestampDisplay = new Date(timestamp / MILLISECONDS_PY).toLocaleDateString();
 
@@ -11,16 +59,7 @@ function Block({ block }) {
         <div className="Block">
             <div>Hash: {hashDisplay}</div>
             <div>Timestamp: {timestampDisplay}</div>
-            <div>
-                {
-                    data.map(transaction => (
-                        <div key={Transaction.id}>
-                            <hr />
-                            <Transaction transaction={transaction}/>
-                        </div>
-                    ))
-                }
-            </div>
+            <ToggleTransactionDisplay block={block} />
         </div>
     )
 }
